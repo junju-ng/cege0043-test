e3dcfe3cae1cb664df5709d6d2efcf17d742cf24
Junju Ng <ngjunju@outlook.com>
2019-02-23 09:33:55 -0500

    add AJAX request and response to testAJAX.js, implemented AJAX in testAJAX.html and added other test html files to try out AJAX

--- testAJAX.js
+++ testAJAX.js
@@ -1,1 +1,19 @@
-// this is an empty file
+var xhr; //define global variable to process AJAX request
+
+// AJAX request function
+function callDivChange(){
+	xhr = new XMLHttpRequest();
+	xhr.open("GET", "test.html", true);
+	xhr.onreadystatechange = processDivChange;
+	xhr.send();
+}
+
+// AJAX response function
+function processDivChange(){
+	if (xhr.readyState < 4) //while waiting for response from server
+		document.getElementById('div1').innerHTML = "Loading...";
+	else if (xhr.readyState === 4) { // 4 = response from server completely loaded
+		if (xhr.status > 199 && xhr.status < 300) // http status between 200 to 299 are all successful
+			document.getElementById('div1').innerHTML = xhr.responseText;
+	}
+}
