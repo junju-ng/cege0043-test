a4f396f2443dd97e4998e58554e3f9ff63b8591b
Junju Ng <ngjunju@outlook.com>
2019-02-24 09:56:56 -0500

    read name, surname and module in form

--- uploadData.js
+++ uploadData.js
@@ -1,4 +1,9 @@
 function startDataUpload(){
-	alert ("start data upload");
+	var name = document.getElementById("name").value; // get name
+	var surname = document.getElementById("surname").value; // get surname
+	var module = document.getElementById("module").value; //get module
+	// create postString with name surname and module
+	var postString = "name=" + name + "&surname=" + surname + "&module=" + module;
+	alert(postString);
 }
 
